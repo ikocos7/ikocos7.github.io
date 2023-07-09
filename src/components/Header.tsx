d30c4c60f6793db8d4497d725a3196af1ee960f7
L.J <yilong.xu@linefriends.com>
2023-07-10 00:52:12 +0900

    feat: add hanzi

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,12 @@ export const Header = (props: IProps) => {
     return <Navbar expand={'sm'} className="bg-body-tertiary mb-3">
         <Container fluid>
             <Navbar.Brand href="/">学汉语</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="/pinyin">拼音练习</Nav.Link>
-            </Nav>
-            <NavDropdown title="学习素材" id="basic-nav-dropdown">
+
+            <NavDropdown align={'end'} title="发音练习" className={'dropdown-menu-end'}>
+                <NavDropdown.Item href={'/pinyin'}>拼音练习</NavDropdown.Item>
+                <NavDropdown.Item href={'/hanzi'}>汉字练习</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown align={'end'} title="学习素材" className={'dropdown-menu-end'}>
                 <NavDropdown.Item href={'/assets/chinese_pinyin_chart_a2_chalkboard.pdf'}>汉语拼音表下载</NavDropdown.Item>
             </NavDropdown>
         </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Pinyin from './pages/Pinyin';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
 import {Error} from "./components/Error";
+import {Hanzi} from "./pages/Hanzi";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 
+
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={<Pinyin/>}></Route>
                 <Route path='/pinyin' element={<Pinyin/>}></Route>
+                <Route path='/hanzi' element={<Hanzi />}></Route>
                 <Route path='*' element={<Error/>}></Route>
             </Routes>
         </BrowserRouter>
